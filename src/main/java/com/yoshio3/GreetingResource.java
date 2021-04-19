088f1ce798e04c1a4526943ea4330183b66e276a
Yoshio Terada <tyoshio2002@yahoo.co.jp>
2021-04-20 03:15:51 +0900

    First Commit

---
+++ src/main/java/com/yoshio3/GreetingResource.java
@@ -0,0 +1,29 @@
+package com.yoshio3;
+
+import java.util.Arrays;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello-resteasy")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        foo();
+        return "Hello RESTEasy";
+    }
+
+    private void foo(){
+        var fruits = Arrays.asList("apple","lemon", "orange");
+
+        Person person = new Person();
+        person.setName("Yoshio");
+        String name = person.getName();
+
+        System.out.println(name);
+    }
+}
